33d19106b10ad6c334e1a92075156ee8c53e6af6
dean_neal <venomsunset@gmail.com>
2018-02-02 14:18:29 +0200

    0.8.8

--- build/demo.js
+++ build/demo.js
@@ -1,8 +1,8 @@
 /*!
- * ace-js 0.8.7
+ * ace-js 0.8.8
  * May be freely distributed under the MIT license 
  * Author: Bogdan Zinkevich
- * Last update: 2018-2-2 14:13:08
+ * Last update: 2018-2-2 14:18:02
  * 
  */
 (function webpackUniversalModuleDefinition(root, factory) {
@@ -1753,7 +1753,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	            var rootEl = document.querySelectorAll(options.root.selector)[0];
 	            if (rootEl) {
 	                var rootComponent = new options.root(rootEl);
-	                rootComponent.root.setAttribute('ac-version', ("0.8.7"));
+	                rootComponent.root.setAttribute('ac-version', ("0.8.8"));
 	            } else {
 	                console.warn('There is no root component');
 	            }
@@ -6205,7 +6205,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	            // });
 
 	            this.props.set({
-	                version: ("0.8.7"),
+	                version: ("0.8.8"),
 	                'categories': [{
 	                    name: 'Getting started',
 	                    items: [{
